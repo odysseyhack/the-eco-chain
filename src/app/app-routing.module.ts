7a9c81e3f65820d49beb748d00c58c6254343089
Albert Hristov <albert.hristov@nl.ibm.com>
2019-04-12 22:40:51 +0200

    add header and user routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
-import { FarmerDashboardComponent } from './farmer-dashboard/farmer-dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FarmerDashboardComponent } from './farmer-dashboard/farmer-dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
   {
     path: 'farmer',
-    component: FarmerDashboardComponent
+    component: FarmerDashboardComponent,
+    children: []
   },
+  {
+    path: 'user',
+    component: UserDashboardComponent,
+    children: []
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FarmerDashboardComponent } from './farmer-dashboard/farmer-dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FarmerDashboardComponent
+    FarmerDashboardComponent,
+    UserDashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
