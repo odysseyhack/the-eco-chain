5b5318ba809ac4416523136a3c73292c3e5a2001
Albert Hristov <albert.hristov@nl.ibm.com>
2019-04-12 22:34:16 +0200

    initial routing farmer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { FarmerDashboardComponent } from './farmer-dashboard/farmer-dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'farmer',
+    component: FarmerDashboardComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
