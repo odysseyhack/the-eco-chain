eddefc364b705d366f3db40aea8eb405a38cf43d
dhrp <t.peskens@nerdalize.com>
2019-04-13 23:46:10 +0200

    Added wizard form

--- src/app/farmer-dashboard/farmer-dashboard.component.ts
+++ src/app/farmer-dashboard/farmer-dashboard.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-farmer-dashboard',
-  templateUrl: './farmer-dashboard.component.html',
-  styleUrls: ['./farmer-dashboard.component.scss']
+  selector: "app-farmer-dashboard",
+  templateUrl: "./farmer-dashboard.component.html",
+  styleUrls: ["./farmer-dashboard.component.scss"]
 })
 export class FarmerDashboardComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
 
+  open: boolean;
+
+  openWizard() {
+    this.open = true;
+  }
 }
--- src/app/farmer-dashboard/form/form.component.ts
+++ src/app/farmer-dashboard/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ClrWizard } from "@clr/angular";
 
 @Component({
   selector: "app-form",
@@ -10,9 +11,59 @@ export class FormComponent implements OnInit {
 
   ngOnInit() {}
 
-  questions = [
-    { label: "Production system" },
-    { label: "Organic or conventional farm" },
-    { label: "Start of reference period" }
-  ];
+  open() {
+    console.log("set open");
+    this._open = !this._open;
+    console.log("it's ", open);
+  }
+
+  @ViewChild("wizardmd") wizardMedium: ClrWizard;
+  _open: boolean = true;
+
+  questions = {
+    crop: [
+      {
+        name: "1.1 Basics",
+        fields: [
+          { label: "Assessment Name", value: "wheat:field-5:fall2019" },
+          { label: "Crop name", value: "wheat" },
+          { label: "Harvest year", value: "2019" },
+          { label: "Crop Area", value: "20 ha" },
+          { label: "Net Yield", value: "90 tonnes" }
+        ]
+      },
+      {
+        name: "1.2 Crop residue management",
+        description:
+          "Default values for dry matter weights are provided for most crops. If you have better data, you can overwrite the default value.",
+        fields: [
+          { label: "Type of co-product", value: "straw" },
+          { label: "Value relative to crop", value: "20%" }
+        ]
+      },
+      {
+        name: "1.3 Co-products",
+        fields: [
+          { label: "Assessment Name", value: "" },
+          { label: "Crop name", value: "wheat" },
+          { label: "Harvest year", value: "2018" },
+          { label: "Crop Area", value: "200" },
+          { label: "Net Yield", value: "" }
+        ]
+      }
+    ],
+    soil: [
+      {
+        name: "2 Field information",
+        fields: [
+          { label: "Soil Texture", value: "" },
+          { label: "Soil organic matter", value: "" },
+          { label: "Soil organic carbon", value: "" },
+          { label: "Soil moisture average", value: "" },
+          { label: "Soil drainage", value: "" },
+          { label: "Soil PH", value: "" }
+        ]
+      }
+    ]
+  };
 }
