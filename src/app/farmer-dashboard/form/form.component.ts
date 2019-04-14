4ec6f387e44c067707c834164e36f0054ae4078c
dhrp <t.peskens@nerdalize.com>
2019-04-14 02:18:25 +0200

    Data table

--- src/app/farmer-dashboard/form/form.component.ts
+++ src/app/farmer-dashboard/form/form.component.ts
@@ -1,61 +1,61 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ClrWizard } from '@clr/angular';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ClrWizard } from "@clr/angular";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.scss"]
 })
 export class FormComponent implements OnInit {
   constructor() {}
 
-  @ViewChild('wizardmd') wizardMedium: ClrWizard;
+  @ViewChild("wizardmd") wizardMedium: ClrWizard;
   // tslint:disable-next-line:variable-name
-  private _open = true;
+  private _open = false;
 
   public questions: object = {
     crop: [
       {
-        name: '1.1 Basics',
+        name: "1.1 Basics",
         fields: [
-          { label: 'Assessment Name', value: 'wheat:field-5:fall2019' },
-          { label: 'Crop name', value: 'wheat' },
-          { label: 'Harvest year', value: '2019' },
-          { label: 'Crop Area', value: '20 ha' },
-          { label: 'Net Yield', value: '90 tonnes' }
+          { label: "Product ID", value: "wheat:field-5:fall2019" },
+          { label: "Crop name", value: "wheat" },
+          { label: "Harvest year", value: "2019" },
+          { label: "Crop Area", value: "20 ha" },
+          { label: "Net Yield", value: "90 tonnes" }
         ]
       },
       {
-        name: '1.2 Crop residue management',
+        name: "1.2 Crop residue management",
         description:
           // tslint:disable-next-line:max-line-length
-          'Default values for dry matter weights are provided for most crops. If you have better data, you can overwrite the default value.',
+          "Default values for dry matter weights are provided for most crops. If you have better data, you can overwrite the default value.",
         fields: [
-          { label: 'Type of co-product', value: 'straw' },
-          { label: 'Value relative to crop', value: '20%' }
+          { label: "Type of co-product", value: "straw" },
+          { label: "Value relative to crop", value: "20%" }
         ]
       },
       {
-        name: '1.3 Co-products',
+        name: "1.3 Co-products",
         fields: [
-          { label: 'Assessment Name', value: '' },
-          { label: 'Crop name', value: 'wheat' },
-          { label: 'Harvest year', value: '2018' },
-          { label: 'Crop Area', value: '200' },
-          { label: 'Net Yield', value: '' }
+          { label: "Assessment Name", value: "" },
+          { label: "Crop name", value: "wheat" },
+          { label: "Harvest year", value: "2018" },
+          { label: "Crop Area", value: "200" },
+          { label: "Net Yield", value: "" }
         ]
       }
     ],
     soil: [
       {
-        name: '2 Field information',
+        name: "2 Field information",
         fields: [
-          { label: 'Soil Texture', value: '' },
-          { label: 'Soil organic matter', value: '' },
-          { label: 'Soil organic carbon', value: '' },
-          { label: 'Soil moisture average', value: '' },
-          { label: 'Soil drainage', value: '' },
-          { label: 'Soil PH', value: '' }
+          { label: "Soil Texture", value: "" },
+          { label: "Soil organic matter", value: "" },
+          { label: "Soil organic carbon", value: "" },
+          { label: "Soil moisture average", value: "" },
+          { label: "Soil drainage", value: "" },
+          { label: "Soil PH", value: "" }
         ]
       }
     ]
@@ -64,8 +64,15 @@ export class FormComponent implements OnInit {
   public ngOnInit(): void {}
 
   public open(): void {
-    console.log('set open');
+    console.log("set open");
     this._open = !this._open;
-    console.log('it\'s ', open);
+    console.log("it's ", open);
+  }
+
+  submitted: boolean = false;
+
+  public doFinish(): void {
+    console.log("do finish");
+    this.submitted = true;
   }
 }
